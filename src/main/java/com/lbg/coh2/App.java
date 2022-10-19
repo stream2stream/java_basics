029c4ff2c121549f32a40fa688954c7270a7c86b
Selvyn <swright@celestial.co.uk>
2022-10-19 14:35:45 +0100

    moved code to using Readers and Streams

--- src/main/java/com/lbg/coh2/App.java
+++ src/main/java/com/lbg/coh2/App.java
@@ -1,14 +1,23 @@
 package com.lbg.coh2;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        System.out.print( "Enter a message: " );
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String dataRead = br.readLine();
+        
+        System.out.println("=> " + dataRead);
     }
-    
 }
